367d4836a3dcfa673e26a78c506b690922b97990
Marcelo Junior <marcelojunior.tech@gmail.com>
2025-12-19 09:16:58 -0300

    feat: Add error handling for uncaught exceptions and unhandled rejections in startup process

--- licenses/src/startup.mjs
+++ licenses/src/startup.mjs
@@ -1,6 +1,20 @@
 import { getLogger, InitLogger } from './utils/logging.mjs';
 import mongoose from 'mongoose';
+import { process } from 'node:process';
 import { database } from './config.mjs';
+
+process.on('uncaughtException', (err) => {
+  console.error('Unhandled Exception:', err);
+  getLogger()?.error(`Unhandled Exception: ${err}`);
+
+  process.exit(-1000); 
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('Unhandled Rejection at:', promise, 'reason:', reason);
+  getLogger()?.error(`Unhandled Rejection at: ${promise} reason: ${reason}`);
+  process.exit(-1001);
+});
 
 export async function jobEntrypoint(cb) {
     try {
